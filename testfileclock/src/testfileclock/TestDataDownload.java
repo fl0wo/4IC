6228b1e5e9862e15a5fe1de5ac685e06329348c0
Giack30 <g.momo.2000@gmail.com>
2017-09-29 15:58:35 +0200

    aggiunta observer thread

--- testfileclock/src/testfileclock/TestDataDownload.java
+++ testfileclock/src/testfileclock/TestDataDownload.java
@@ -5,55 +5,142 @@
  */
 package testfileclock;
 import java.util.Date;
+
 import java.util.concurrent.TimeUnit;
 
+
+
 public class TestDataDownload {
+
+
+
     public static void main(String[] args) {
+
         DataDownloader ddl = new DataDownloader();
-        Thread td = new Thread(ddl,"DataDownloaderThread");
-        Thread tn = new NetworkConnection(td,"NetworkConnectionThread");
+
+        Thread td = new Thread(ddl, "DataDownloaderThread");
+
+        Thread tn = new NetworkConnection(td, "NetworkConnectionThread");
+
         tn.start();
+
         td.start();
+
     }
+
 }
+
 
 
 class DataDownloader implements Runnable {
-    
+
+
+
     @Override
+
     public void run() {
-        System.out.printf("Connecting: %s\n",
-                           new Date());
+
+        System.out.printf("Connessione: %s\n",new Date());
+
         try {
-            
-            TimeUnit.SECONDS.sleep(10);
+
+            TimeUnit.SECONDS.sleep(4);
+
         } catch (InterruptedException e) {
+
             e.printStackTrace();
+
         }
-        System.out.printf("Download finished: %s\n", 
-                           new Date());
+
+        System.out.printf("Download finito: %s\n",new Date());
+
     }
+
 }
 
+
+
 class NetworkConnection extends Thread {
-    
-    private Thread td; // the downloadr
-    
-    NetworkConnection(Thread td, String name){
+
+
+
+    private Thread td; 
+
+
+
+    NetworkConnection(Thread td, String name) {
+
         super(name);
+
         this.td = td;
+
     }
-    
+
+
+
     @Override
+
     public void run() {
-        System.out.printf("Network connected: %s\n",
-                           new Date());
+
+        System.out.printf("Network connessa: %s\n", new Date());
+
         try {
-            td.join();
+
+            Thread observer = new Thread(new Observ(Thread.currentThread()));       //create a new thread 
+            observer.start();    
+            td.join();  
+
         } catch (InterruptedException e) {
+
             e.printStackTrace();
+
         }
-        System.out.printf("Network not connected: %s\n", 
-                           new Date());
+
+        System.out.printf("Network non connessa: %s\n",new Date());
+
     }
+
+
+
+    public class Observ implements Runnable {
+
+
+        private Thread Observable;
+
+
+        /**
+         * 
+         * @param currentThread the thread that u want to see the states
+         */
+        private Observ(Thread currentThread) {
+        Observable = currentThread;
+        }
+
+        @Override
+
+        public void run() {
+
+
+
+            if (Observable.isAlive()) {    
+                System.out.println(Observable.getState().name());   //if thread is not killed, it prints the actual state of the thread
+
+            }
+            /**
+             * untill the thread is running it will do nothing
+             * or it will print it a lot of times
+             */
+            while(Observable.getState()!=Thread.State.BLOCKED){
+
+            }
+                //print the state when it change
+             System.out.println(Observable.getState().name());
+
+        }
+
+
+
+    }
+
 }
+
