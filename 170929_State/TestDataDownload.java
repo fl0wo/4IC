efca40525d7e6f10a9ce6ec85182d49f9d594ad3
ObZenTish <pietro.cap@outlook.com>
2017-09-29 15:16:21 +0200

    UPDATED PROJECT
    https://i.imgur.com/nWuT2Ta.png

--- 170929_State/TestDataDownload.java
+++ 170929_State/TestDataDownload.java
@@ -1,79 +1,105 @@
+
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class TestDataDownload {
+
     public static void main(String[] args) {
         DataDownloader ddl = new DataDownloader();
-        Thread td = new Thread(ddl,"DataDownloaderThread");
-        Thread tn = new NetworkConnection(td,"NetworkConnectionThread");
-        System.out.println("Data Downloader Status :"+td.getState());
-        System.out.println("Network Connection Status :"+tn.getState());
-        tn.start();
-        System.out.println("Data Downloader Status :"+tn.getState());
-        td.start();
-        System.out.println("Network Connection Status :"+td.getState());
-         try {
-            TimeUnit.SECONDS.sleep(4);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Data Downloader Status :"+tn.getState());
-        System.out.println("Network Connection Status :"+td.getState());
-         try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Data Downloader Status :"+tn.getState());
-        System.out.println("Network Connection Status :"+td.getState());
         
+        Thread td = new Thread(ddl, "DataDownloaderThread");
+        Thread tn = new NetworkConnection(td, "NetworkConnectionThread");
+
+        Watcher cambiamenti = new Watcher(td, tn);
+        cambiamenti.start();
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
 
 class DataDownloader implements Runnable {
-    
+
     @Override
     public void run() {
         System.out.printf("Connecting: %s\n",
-                           new Date());
+                new Date());
         try {
             TimeUnit.SECONDS.sleep(4);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.printf("Download finished: %s\n", 
-                           new Date());
+        System.out.printf("Download finished: %s\n",
+                new Date());
     }
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 class NetworkConnection extends Thread {
-    
-    private Thread td; // the downloadr
-    
-    NetworkConnection(Thread td, String name){
+
+    private Thread td;
+
+    NetworkConnection(Thread td, String name) {
         super(name);
         this.td = td;
     }
-    
+
     @Override
     public void run() {
         System.out.printf("Network connected: %s\n",
-                           new Date());
+                new Date());
         try {
             td.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.printf("Network not connected: %s\n", 
-                           new Date());
+        System.out.printf("Network not connected: %s\n",
+                new Date());
     }
 }
 
-class Watcher extends Thread{
-	
-	Watcher(Thread td,Thread tn){
-		
-	}
+////////////////////////////////////////////////////////////////////////////////
+
+class Watcher extends Thread {
+
+    Thread data;
+    Thread network;
+
+    Watcher(Thread td, Thread tn) {
+        this.data = td;
+        this.network = tn;
+    }
+
+    public void toStringData() {
+        System.out.println("Data Downloader Status :" + data.getState());
+    }
+
+    public void toStringNet() {
+        System.out.println("Network Connection Status :" + network.getState());
+    }
+   
+    @Override
+    public void run() {
+        this.toStringData();
+        this.toStringNet();
+        network.start();
+        data.start();
+        this.toStringData();
+        this.toStringNet();
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.toStringData();
+        this.toStringNet();
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.toStringData();
+        this.toStringNet();
+    }
 
 }
